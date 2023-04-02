83b6dd90695990c5cea9ee166224b8ce1d43b787
Samantha Vale <karlasamantha@gmail.com>
2023-04-02 15:40:05 -0700

    chore: adjusts response type on request

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,6 +1,11 @@
+import { ShrtcodeResponseType } from '../types'
+
 export const SHRTCODE_API: string = `https://api.shrtco.de/v2/shorten?url=`
 
-export default async function request(query: string, options = {}) {
+export default async function request(
+  query: string,
+  options = {}
+): Promise<ShrtcodeResponseType> {
   const res = await fetch(`${SHRTCODE_API}${query}`, {
     ...options,
     method: 'GET'
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,23 @@
-export interface ShrtcodeResponse {
-    code: string,
-    short_link: string,
-    full_short_link: string,
-    short_link2: string,
-    full_short_link2: string,
-    share_link: string,
-    full_share_link: string,
-    original_link: string
+export type ShrtcodeResultType = {
+  code: string
+  short_link: string
+  full_short_link: string
+  short_link2: string
+  full_short_link2: string
+  share_link: string
+  full_share_link: string
+  original_link: string
+}
+
+export type ShrtcodeResponseType = {
+  ok: boolean
+  result: ShrtcodeResultType
 }
 
 export interface ChildrenProps {
   children: string | JSX.Element
+}
+
+export interface ShortenedURLsProps {
+  urls: [] | ShrtcodeResultType[]
 }
