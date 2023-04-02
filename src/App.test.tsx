8236cc7fdb4c9395fd9115e46d3f7f080e8f2c5b
Samantha Vale <karlasamantha@gmail.com>
2023-04-02 15:48:25 -0700

    test: updates basic test on App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders form input', () => {
+  render(<App />)
+  const linkElement = screen.getByPlaceholderText(/Enter URL here/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test('it should render the Loading component when submitting the form', async () => {
+  render(<App />)
+  const inputElement = screen.getByPlaceholderText('Enter URL here')
+  const submitButton = screen.getByRole('button', { name: 'Shorten URL' })
+  fireEvent.change(inputElement, { target: { value: 'https://www.test.com' } })
+  fireEvent.click(submitButton)
+  expect(screen.getByTestId('loading')).toBeInTheDocument()
+})
+
+test('it should render the Error component after unsuccessful API request', async () => {})
