d6f8ad49126a351ff34488ce79df80aa1bea5cb1
Samantha Vale <karlasamantha@gmail.com>
2023-04-02 18:12:19 -0700

    fix: adjuts styles for CurrentURL component

--- src/components/CurrentURL.tsx
+++ src/components/CurrentURL.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { CurrentURLProps } from '../types'
+import '../styles/CurrentURL.css'
 
 function CurrentURL({ shortURL, originalURL }: CurrentURLProps) {
+  const [isCopied, setIsCopied] = React.useState<boolean>(false)
+
+  function handleCopy() {
+    setIsCopied(true)
+    setTimeout(async () => {
+      await navigator.clipboard.writeText(shortURL)
+      setIsCopied(false)
+    }, 900)
+  }
+
   return (
-    <div>
-      <p>{shortURL}</p>
-      <p>{originalURL}</p>
+    <div className="container">
+      <div className="current-url">
+        <input type="text" value={shortURL} readOnly />
+        <button onClick={handleCopy}>
+          <span>{isCopied ? 'Copied!' : 'Copy'}</span>
+        </button>
+      </div>
+      <span>
+        Original URL: <a href={originalURL}>{originalURL}</a>
+      </span>
     </div>
   )
 }
