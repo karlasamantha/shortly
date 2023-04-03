b58b35bf77d9143060b884be8d53305d54b3de3d
Samantha Vale <karlasamantha@gmail.com>
2023-04-02 17:58:32 -0700

    fix: fixes styles for Loading and form element

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import '../styles/Loading.css'
 
 function Loading() {
-  return <div data-testid="loading"></div>
+  return (
+    <div data-testid="loading" className="loading-container">
+      <div className="spinner"></div>
+    </div>
+  )
 }
 
 export default Loading
